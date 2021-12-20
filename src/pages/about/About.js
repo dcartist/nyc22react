f036f3393bf9d7a02ae7782b28d45187c034c954
dcartist <pbannerman13@gmail.com>
2021-12-19 20:11:58 -0500

    updating about

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import helmet from "../../images/photos/helmets.png"
 // import "animate.css/animate.min.css";
+import 'animate.css';
+import ScrollAnimation from 'react-animate-on-scroll';
+
 export default function About() {
     return (
         <div className='about'>
             <div>
+            <ScrollAnimation duration={3} animateIn="fadeIn" className='animate__fadeIn' >
             <img src={helmet}/>
+</ScrollAnimation>
+          
                 </div>
             <div><p>NYC22 is a demo application based on the New York Department of Buildings (DOB)'s job application filings. It contains all job applications submitted through the Borough Offices, through eFiling, or the HUB, from January 1, 2000, to 2019.</p>
           <p>In this react node application, the users can view the demo as either a contract or a construction job client.</p>
+         
           <h2>Technology Used</h2>
 <ul>
     <li>Node</li>
