643e7504a6a144d4950955c23dd83004493a220b
dcartist <pbannerman13@gmail.com>
2022-01-22 18:55:36 -0500

    added axios

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import './style.scss';
+import React, { useState, useEffect } from 'react';
 import {Route, Link, Redirect, Switch} from "react-router-dom";
 import Home from "./pages/intro/Intro"
 import About from "./pages/about/About"
 import AboutMain from "./pages/about/AboutMain"
 import Dashboard from "./pages/dashboard/Client"
 import Zombies from "./pages/dashboard/Zombie"
-
+import axios from 'axios'
 function App() {
  
- 
+  // const getGames = () => {
+  //   return fetch('http://localhost:3001/games/').then(response => response.json());
+  // };
+  
+
+  useEffect(() =>
+  axios.get("https://ancient-fjord-10674.herokuapp.com/").then(res => console.log(res.data))
+  // .then(setBook)
+    
+)
 
   return (
     <div className="main">
