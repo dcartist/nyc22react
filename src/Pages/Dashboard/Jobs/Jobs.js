d680ad742723488fe87b82751672b6c8b4508c83
dcartist <pbannerman13@gmail.com>
2025-10-14 15:16:56 -0400

    changing pagination

--- src/Pages/Dashboard/Jobs/Jobs.js
+++ src/Pages/Dashboard/Jobs/Jobs.js
@@ -1,20 +1,29 @@
 import React, {useEffect, useState} from 'react'
 
 import JobsTable from '../../../Components/Tables/Jobs/JobsTable';
-import  {getAllJobs, getMetadata} from '../../../services/api';
+import  {getAllJobs, getMetadata, getPaginatedJobs} from '../../../services/api';
 import JobsTablePagination from '../../../Components/Tables/Jobs/JobsTablePagination';
 
 export default function Jobs() {
   const [jobs, setJobs] = useState([]);
+  const [paginationJobs, setPaginationJobs] = useState([]);
   const [jobamount, setJobAmount] = useState(0);
 
   useEffect(() => {
-    const fetchJobs = async () => {
-      const data = await getAllJobs();
+    const fetchPaginationJobs = async () => {
+      const data = await getPaginatedJobs(1, 100);
+      setPaginationJobs(data);
       setJobs(data);
-      console.log("Jobs data:", data);
+      console.log("Paginated Jobs data:", data);
     };
-    fetchJobs();
+    fetchPaginationJobs();
+
+    // const fetchJobs = async () => {
+    //   const data = await getAllJobs();
+    //   setJobs(data);
+    //   console.log("Jobs data:", data);
+    // };
+    // fetchJobs();
 
     const fetchMetadata = async () => {
       const metadata = await getMetadata();
